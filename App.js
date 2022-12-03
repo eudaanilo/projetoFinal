55fb2e7edec29fe1dac0637a2856767d4aeb628a
eudaanilo <dsa7@aluno.ifal.edu.br>
2022-12-03 18:15:59 -0300

    Modificado o estilo do css

--- App.js
+++ App.js
@@ -6,15 +6,15 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 export default function App(){
 
 
-  function fotoResumo(){
+  function FirstPage(){
     return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.FirstPage}>
         <Text>Aqui vai ficar minha foto de perfil e um breve resumo sobre mim \o/</Text>
       </View>
     );
   }
 
-  function textoSobre() {
+  function SecondPage() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Aqui ficará um texto sobre mim \o/</Text>
@@ -22,7 +22,7 @@ export default function App(){
     );
   }
 
-  function vidaAcademica() {
+  function ThirdPage() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Aqui ficará tudo sobre minha vida acadêmica \o/</Text>
@@ -30,7 +30,7 @@ export default function App(){
     );
   }
 
-  function oQueGosto() {
+  function FourthPage() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Aqui ficará tudo o que eu gosto \o/</Text>
@@ -38,10 +38,10 @@ export default function App(){
     );
   }
 
-  function fim() {
+  function LastPage() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Aqui será o fim :(</Text>
+        <Text>Aqui será o fim :</Text>
       </View>
     );
   }
@@ -50,11 +50,11 @@ export default function App(){
   return(
     <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Foto de perfil" component={fotoResumo}/>
-          <Tab.Screen name="Texto sobre mim" component={textoSobre}/>
-          <Tab.Screen name="Vida acadêmica" component={vidaAcademica}/>
-          <Tab.Screen name="O que gosto" component={oQueGosto}/>
-          <Tab.Screen name="Fim" component={fim}/>
+          <Tab.Screen name="Foto de perfil" component={FirstPage}/>
+          <Tab.Screen name="Texto sobre mim" component={SecondPage}/>
+          <Tab.Screen name="Vida acadêmica" component={ThirdPage}/>
+          <Tab.Screen name="O que gosto?" component={FourthPage}/>
+          <Tab.Screen name="Fim" component={LastPage}/>
         </Tab.Navigator>
     </NavigationContainer>
   );
@@ -62,21 +62,14 @@ export default function App(){
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
-  fadingContainer: {
-    padding: 20,
-    backgroundColor: "powderblue"
+  FirstPageflex:{
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  fadingText: {
-    fontSize: 28
-  },
-  buttonRow: {
-    flexBasis: 100,
-    justifyContent: "space-evenly",
-    marginVertical: 16
-  }
+
 });
