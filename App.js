1183676d680335e84fa228cf756a034272f45102
eudaanilo <dsa7@aluno.ifal.edu.br>
2022-12-05 16:00:40 -0300

    Adicionado algumas estilizações e algumas imagens

--- App.js
+++ App.js
@@ -6,6 +6,29 @@ import { Entypo } from '@expo/vector-icons'
 
 export default function App(){
 
+  function Welcome(){
+    return(
+      <View style={styles.body}>
+        <Text>Laboratório de Aplicativos</Text>
+
+
+        <View style={styles.containerLogo}>
+          <Image source={require('./assets/cinfo.png')} style={{ width: '100%' }} resizeMode='contain'/>
+        </View>
+
+        <View style={styles.containerForm}>
+          <Text>Laboratório de Aplicativos Ifal Maceió</Text>
+          <Text> Coordenação de Iformática</Text>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+
   function FirstPage() {
     return(
       <View style={styles.body}>
@@ -64,10 +87,15 @@ export default function App(){
           eu sempre gostei de coisas complicadas, mas não ao ponto de me fazer desistir, tipo
           esse App. Eu penso que coisas complicadas são para pessoas mais intelectuais, e isso
           é bom… Eu estou fazendo como se fosse algumas especializações em Desenvolvimento
-          de Sistemas, vulgo informática, para fazer de tudo um pouco… A área que eu mais 
+          de Sistemas –vulgo informática– para fazer de tudo um pouco… A área que eu mais 
           gostei da mesma foi em montagem e reparação e a parte de front-end, por mais que 
-          react native trabalhe tanto com a parte de front-end e também de back-end eu 
-          gostei de trabalhar com a linguagem. 
+          React Native trabalhe tanto com a parte de front-end e também de back-end eu 
+          gostei de trabalhar com a linguagem. Eu entrei no IFAL durante a pandemia, então
+          imaginem, a maior parte do ensino médio foi online, e como todo mundo sabe foi
+          difícil pra todo mundo, já que todo mundo não tinha condições de ter um computador
+          em casa, e, alem desse problema, tinha o de sempre dormir nas aulas, e o barulho
+          que era de panela batendo, gente falando, brigando ou conversando. Esse fato serviu
+          para mostrar o quanto a internet e a informática é importante em nossa vida  
         </Text>
       </View>
     );
@@ -215,5 +243,46 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
     textAlign: 'justify',
-  }
+    marginLeft: 5,
+    marginRight: 10,
+    marginTop: 5,
+  },
+  containerLogo:{
+    flex: 2,
+    backgroundColor:'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerForm:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 12,
+  },
+  text:{
+    color: '#a1a1a1'
+  },
+  button:{
+    position:'absolute',
+    backgroundColor: '#38a69d',
+    borderRadius: 50,
+    paddingVertical: 8,
+    width: '60%',
+    alignSelf:'center',
+    bottom: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight:'bold'
 });
